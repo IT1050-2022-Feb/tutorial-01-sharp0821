aeeabe9f0e27d8e31f4b0047bd65fc46b692c8d3
praveen <94636505+sharp0821@users.noreply.github.com>
2022-02-09 15:35:41 -0800

    TUTE 01 ,Exercise 1 - Calculations

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main() {
+      int mark,i,total=0;
+   
+   for(i=1;i<3;i++){ 
+      printf("Enter your marks:\n");
+      scanf("%d",&mark);
+      
+      total+=mark;
+   }
+   printf("Average mark is:%d",total/i);
   
   return 0;
 }
