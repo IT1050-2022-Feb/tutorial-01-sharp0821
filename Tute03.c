992d99f13c65fbd471b6229232fa85317f02cea8
praveen <94636505+sharp0821@users.noreply.github.com>
2022-02-10 11:05:33 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -11,9 +11,16 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
-int main() {
-  
-  return 0;
-}
+#include<stdio.h>
+int main(){
+	int value=1,sum=0;
+	while (value >0){
 
+	printf("Enter number:");
+	scanf("%d",&value);
+	sum+=value;
+	
+		}
+	printf("Sum of number is:%d",sum);
+	return 0;
+}
